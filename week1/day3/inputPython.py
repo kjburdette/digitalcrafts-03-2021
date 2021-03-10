2bffcae99d1b20c533244c899223064e895b11cf
Kendall Burdette <kjburdette2015@gmail.com>
2021-03-10 12:53:44 -0500

    saved input info

---
+++ week1/day3/inputPython.py
@@ -0,0 +1,4 @@
+name_of_user = input("What is your first name?")
+# nameOfUser = input("What is your name?")
+lastNameOfUser = input("What is your last name?")
+print("Hello %s %s, welcome to python" % (name_of_user, lastNameOfUser))
--- week1/day3/pythonExamples.py
+++ week1/day3/pythonExamples.py
@@ -1,6 +1,10 @@
 gretting = "Hello World"
 classSize = 23
+somethingToCallIt = ["students", 23, True]
+somethingToCallIt = {"helloPythonWorld": 23, "we are students": 24.5}
 
 print("Hello World")
 print("I'm Kendall from Atlanta, GA.")
 print(classSize)
+print(somethingToCallIt)
+print(somethingToCallIt)
