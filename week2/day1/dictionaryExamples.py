6d3d62ccf81e69a52841284b8c217a3f85e75a3e
Kendall Burdette <kjburdette2015@gmail.com>
2021-03-15 14:14:15 -0400

    Created dictionary examples

---
+++ week2/day1/dictionaryExamples.py
@@ -0,0 +1,42 @@
+digitalCraftsStudent = {
+    "name": "",
+    "city": "",
+    "computer": [],
+
+}
+
+gamer = {
+    "platform": "",
+    "gamingHours": [{"weekday": "9-5"}, {"weekends": "anytime"}],
+    "skill": ''
+}
+
+car = {
+    "model": "",
+    "image": "",
+    "year": "",
+    "engineChoices": [
+
+        {
+            "v4": {
+                "horsepower": 230
+            }
+        },
+        {
+            "v6": {
+                "horsepower": 300
+            }
+        },
+        {
+            "v8": {
+                "horsepower": 400
+            }
+        },
+        {
+            "v12": {
+                "horsepower": 900
+            }
+        }
+    ],
+
+}
