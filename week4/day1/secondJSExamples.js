a2a66d30c72abad44110cccbeeb3cf0edf602456
Kendall Burdette <kjburdette2015@gmail.com>
2021-03-29 15:32:49 -0400

    more JS examples with objects and arrays

---
+++ week4/day1/secondJSExamples.js
@@ -0,0 +1,27 @@
+// string interpolation
+
+// let height = "6ft"
+// let weight = "140lbs"
+// let JoesDimensions = `Joes height is ${height} and he weights ${weight}.`
+// console.log(JoesDimensions)
+
+// for in is for objects
+// const singlePizza = {
+//     crust: 'thin',
+//     toppings: ["cheese", "pepperoni", "spinach", "onion", "anchovies", "feta", "seaweed"],
+//     size: "large",
+//     sauce: ["marinara", "alfredo"]
+
+// }
+// for(const pizza in singlePizza){
+//     console.log(singlePizza[pizza]);
+// }
+
+
+// for of is for arrays
+
+let cars = ["Subaru", "Audi", "Honda", "BMW"]
+
+for (const car of cars) {
+    console.log("this is a", car);
+}
