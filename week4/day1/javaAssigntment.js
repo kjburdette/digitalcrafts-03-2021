a0fbe6109c9d758deed8ce39681b56aa0d379087
Kendall Burdette <kjburdette2015@gmail.com>
2021-03-29 14:06:49 -0400

    Java examples

---
+++ week4/day1/javaAssigntment.js
@@ -0,0 +1,85 @@
+// Define Variables
+
+// define an array in javascript
+
+// define an object in javascript
+
+// define an integer
+
+// define a string
+
+// re-assign the string you defined above to "This is a new string"
+
+// create a new variable that takes your integer above and multiplies it by 5
+
+// Create an array that has the following in it ["swag", 123123, true, false, "boolean"]
+
+// print out 123123 print out false print out boolean bonus: print out the length of the array
+
+// -Create an object that has the following structure name: "name", age: 20, height: 7.1, metric: "ft"
+
+// You will create 5 objects with unique names, with unique ages, height.
+// Print out the first objects name
+// Print out the last objects age
+// print out the 3rd objects height
+// change the 3rd objects height to be 1.2
+// change the 3rd objects metric to be "meters"
+// create a new key on the 2nd object called sport. Assign it your favorite sport
+
+// let variableName = Kendall
+
+// array = []
+// object = {key : "value"}
+// integer = 2
+// string = "String"
+// string = "This is a new string"
+// newInteger = integer * 5
+
+// arrayExample = ["swag", 123123, true, false, "boolean"]
+// console.log(arrayExample[1])
+// console.log(arrayExample[3])
+// console.log(arrayExample[4])
+// console.log(arrayExample.length)
+
+// let objectExample = {name: "name", age: 20, height: 7.1, metric: "ft"}
+
+let objectExercise = {
+    jessica: {
+        name: "jessica",
+        age: 20,
+        height: 5.6,
+        metric: "ft"
+      },
+      whitney: {
+        name: "whitney",
+        age: 21,
+        height: 5.3,
+        metric: "ft"
+      },
+      tay: {
+        name: "tay",
+        age: 22,
+        height: 5.4,
+        metric: "ft"
+      },
+      joey: {
+        name: "joey",
+        age: 23,
+        height: 6.1,
+        metric: "ft"
+      },
+      michael: {
+        name: "michael",
+        age: 24,
+        height: 6.0,
+        metric: "ft"
+}
+}
+
+console.log(objectExercise.jessica.name)
+console.log(objectExercise.michael.age)
+console.log(objectExercise.tay.height)
+objectExercise.tay.height = 1.2
+objectExercise.tay.metric = "meters"
+objectExercise.whitney.sport = "soccer"
+console.log(objectExercise.whitney.sport)
