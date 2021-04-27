7071106aa7a84bbf681300e29cc5d16ba7b58650
Kendall Burdette <kjburdette2015@gmail.com>
2021-04-27 10:07:24 -0400

    added hw db

---
+++ week8/day1/assignment2/server/index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3002;
+const pool = require("./db.js");
+const { readFile } = require("fs");
+const es6Renderer = require("express-es6-template-engine");
+
+// middelware
+app.use(cors());
+app.use(express.json());
+app.engine("html", es6Renderer);
+app.set("views", "../template");
+app.set("view engine", "html");
+
+app.get("/", (req,res) => {
+    res.render("home");
+})
+
+// create todo
+app.post("/todo", async (req,res) => {
+    try {
+        const {description} = req.body;
+        const newTodoInDB = await pool.query("INSERT INTO todo (description) VALUES($1)", [description]);
+        console.log(req.body);
+        res.json(newTodoInDB);
+    } catch (err) {
+        console.log(err.message)
+    }
+})
+
+// read the todo list
+app.get("/read_todos", async (req,res) => {
+    try{
+        const todolist = await pool.query("Select * from todo").then((data) => {
+         console.log(data)
+        })
+
+    }catch (err){
+        console.log(err.message)
+    }
+});
+        
+
+
+// read a single todo
+app.get("/read_todos/:id", async (req,res) => {
+    try {
+        const {id} = req.params;
+       const readSingleTodoFromDB = await pool.query("Select * from todo WHERE todo_id = ($1)", [id]);
+       res.json(readSingleTodoFromDB)
+    } catch (err) {
+        console.log(err.message)
+    }
+})
+
+// update the todo item on todolist
+app.put("/update_todos/:id", async (req,res) => {
+    try{
+        const {id} = req.params;
+        const {description} = req.body;
+        const updateTodoInDB = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+    
+        res.json("Updated the todos!")
+
+    }catch (err){
+        console.error(err);
+    }
+})
+
+// delete the todo
+app.delete("/delete_todo/:id", async (req,res) => {
+    try{
+        const {id} = req.params;
+        const deleteTodoInDB = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+        res.json("Todo was successfuly deleted!")
+
+    }catch (err){
+        console.error(err.message)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Your server is being hosted on localhost:${port}`)
+})
