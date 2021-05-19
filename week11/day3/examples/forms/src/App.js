6d8ae6de4774fdbd7d101a558c53b17bf0c401c7
Kendall Burdette <kjburdette2015@gmail.com>
2021-05-19 16:36:24 -0400

    view shipping form

--- week11/day3/examples/forms/src/App.js
+++ week11/day3/examples/forms/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       {/* Uncomment out form to see the new bigger shipping form */}
-      {/* <Form /> */}
+      <Form />
       <CarlosForm />
     </div>
   );
