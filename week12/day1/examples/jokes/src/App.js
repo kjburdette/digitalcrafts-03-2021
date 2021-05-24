b0bcc9feb2d7ffc50f6e04ea16ee56ca312cbe4a
Kendall Burdette <kjburdette2015@gmail.com>
2021-05-24 14:32:46 -0400

    use effects

---
+++ week12/day1/examples/jokes/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import JokeContainer from './Components/JokeContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <JokeContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ week12/day1/examples/jokes/src/Components/Jokes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function Jokes(props) {
+    return (
+        <div>
+            <li>{props.joke}</li>
+        </div>
+    )
+}
